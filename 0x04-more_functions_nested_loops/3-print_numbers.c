3f9f04b77616be04be4a93b7643373bd54d6fffb
KabzaKole <kabelomasela>
2023-02-23 05:38:39 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers-checks for a digit (0 through9)
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 18; c < 28; c++)
+	{
+	_putchar(c);
+	}
+	-putchar("\n");
+}

45fd4bcb8636733714bd086bd0e48441892f02e7
KabzaKole <kabelomasela>
2023-03-14 04:07:16 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,12 @@
 * @argv: array of arguments
 * Return: Always 0 (Success)
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
+
 {
-	printf("%d\n", *argc);
 
+(void) argv; /*Ignore argv*/
+
+	printf("%d\n", argc - 1);
 	return (0);
-
 }

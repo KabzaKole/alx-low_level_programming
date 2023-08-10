18ad01521400bc21ddc31d6b70a71b6983cf126b
KabzaKole <kabelomasela47@gmail.com>
2023-08-10 16:19:04 -0700

    0-malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *malloc_checked - a function with a void return type that
+ * allocates memory using malloc
+ * @b:the number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *memallo = malloc(b);
+
+	if (memallo == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(98);
+	}
+	return (memallo);
+}

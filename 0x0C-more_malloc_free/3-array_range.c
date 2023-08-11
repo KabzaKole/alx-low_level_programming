c2a361043c1d29ace21994f7edea51a9137072b7
KabzaKole <kabelomasela47@gmail.com>
2023-08-10 17:33:48 -0700

    Updated array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * array_range - Creates an array of integers ordered
@@ -10,27 +9,22 @@
  * Return: If min > max or the function fails - NULL.
  *         Otherwise - a pointer to the newly created array.
  */
-
 int *array_range(int min, int max)
 {
+	int *array, index, size;
+
 	if (min > max)
-	{
 		return (NULL);
-	}
 
-	unsigned in size = max - min + 1;
+	size = max - min + 1;
 
-	int *arr_ran = malloc(size * sizeof(int));
+	array = malloc(sizeof(int) * size);
 
-	if (arr_ran == NULL)
-	{
+	if (array == NULL)
 		return (NULL);
-	}
 
-	for (unsigned int i = 0; i < size; i++)
-	{
-		arr_ran[i] = min + i;
-	}
+	for (index = 0; index < size; index++)
+		array[index] = min++;
 
-	return (arr_ran);
+	return (array);
 }

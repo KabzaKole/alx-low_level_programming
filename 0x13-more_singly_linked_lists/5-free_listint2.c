ca859d75240328918c8fb9980db62c7a12c8fc0e
KabzaKole <kabelomasela47@gmail.com>
2023-08-28 14:06:16 -0700

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees up a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+
+void free_listsint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		current = (*head)->next;
+		current(*head);
+		*head = current;
+
+	}
+	*head = NULL;
+}

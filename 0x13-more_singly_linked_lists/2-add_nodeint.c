ce9a711be4d4d304f6912ed5e80d166a3cf68e97
KabzaKole <kabelomasela47@gmail.com>
2023-08-28 12:16:46 -0700

    Resolved issues with add doe

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,11 +5,13 @@
  * @head: pointer to the first node in the list
  * @n: data to insert in that new node
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: pointer to the new node or NULL if it fails
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *add_node;
+
 	add_node = malloc(sizeof(listint_t);
 
 	if (add_node == NULL)

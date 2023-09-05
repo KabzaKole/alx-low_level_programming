822b3e8502bf36ea9a2a3b5c0ac8899f6a27787d
KabzaKole <kabelomasela47@gmail.com>
2023-09-04 19:24:53 -0700

    Rsolved create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,17 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		if (o == -1)
-		return (-1);
-
-		close(o)
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	for (len = 0; text_content[len];)
-		len++;
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
